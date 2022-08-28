144bea2b080817d4c17c79712b1154f8c37cc651
Badruzzaman <badru.cse@gmail.com>
2022-08-28 19:54:33 +0600

    Add project files.

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int[] array = { 8, 5, 2, 9, 5, 6, 3 };
+int[] result = InsertionSort(array);
+foreach (var item in result)
+{
+	Console.Write(" " + item);
+}
+Console.ReadKey();
+
+int[] InsertionSort(int[] array)
+{
+	for (int i = 1; i < array.Length; i++)
+	{
+		int j = i;
+		while (j > 0 && array[j] < array[j-1])
+		{
+			Swap(j, j - 1, array);
+			j--;
+		}
+	}
+    return array;
+}
+void Swap(int i, int j, int[] array)
+{
+	int temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
+}
